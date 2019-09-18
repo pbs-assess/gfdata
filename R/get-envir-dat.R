847e4d845edae8c2d5174761ccc419e6a3d61812
Sean Anderson <sean@seananderson.ca>
2019-09-18 12:18:02 -0700

    Fix up some documentation documentation

--- R/get-envir-dat.R
+++ R/get-envir-dat.R
@@ -1,18 +1,36 @@
-#' Environmental data extraction for sensors deployed on trawl survey gear
+#' Environmental data extraction functions from surveys
+#'
+#' Environmental data extraction functions from surveys. See Details section.
+#'
+#' @details `get_sensor_data_trawl()`: Environmental data extraction for sensors
+#' deployed on trawl survey gear
 #'
 #' @param ssid A numeric vector of survey series IDs. Run [get_ssids()] for a
 #'   look-up table of available survey series IDs with surveys series
 #'   descriptions.
-#' @param attribute A character vector of sensor attributes to filter for.
-#'   Run `get_sensor_attributes()` for a look-up table of available attributes.
+#' @param attribute A character vector of sensor attributes to filter for. Run
+#'   [get_sensor_attributes()] for a look-up table of available attributes.
 #' @param spread_attributes Logical for whether the attributes should be
-#'   returned in a wider format. Allows for user to choose whether data are output in
-#'   wide format (= TRUE) with min and max values for each attribute for each
-#'   fishing event, or in long format (= FALSE) with only mean values for each
-#'   attribute and fishing event.
+#'   returned in a wider format. Allows for user to choose whether data are
+#'   output in wide format (`TRUE``) with min and max values for each attribute
+#'   for each fishing event, or in long format (`FALSE``) with only mean values
+#'   for each attribute and fishing event.
 #'
 #' @export
 #' @rdname get_environmental_data
+#'
+#' @examples
+#' d <- get_sensor_data_trawl(ssid = 1, "temperature")
+#' head(d)
+#' head(get_sensor_data_trawl_fe(d$fishing_event_id[[1]], "temperature"))
+#'
+#' d <- get_sensor_data_ll_td(ssid = 40)
+#' head(d)
+#' head(get_sensor_data_ll_td_fe(d$fishing_event_id[[1]],"temperature"))
+#'
+#' d <- get_sensor_data_ll_ctd(40)
+#' head(d)
+
 get_sensor_data_trawl <- function(ssid = NULL,
   attribute = c("temperature", "depth", "dissolved oxygen", "salinity"),
   spread_attributes = FALSE) {
@@ -32,7 +50,7 @@ get_sensor_data_trawl <- function(ssid = NULL,
   names(.d) <- tolower(names(.d))
   .d$attribute <- tolower(.d$attribute)
   .d$attribute <- gsub("dissolved oxygen", "do", .d$attribute)
-  .d <- unique(.d)
+  .d <- dplyr::distinct(.d)
   .d <- .d %>%
     dplyr::mutate(attribute = paste0(attribute, "_", unit)) %>%
     dplyr::select(-unit)
@@ -47,9 +65,10 @@ get_sensor_data_trawl <- function(ssid = NULL,
   tibble::as_tibble(.d)
 }
 
-#' Environmental data extraction for trawl surveys, for individual fishing events
+#' @details
+#' `get_sensor_data_trawl_fe()`: Environmental data extraction for trawl
+#' surveys, for individual fishing events.
 #'
-#' @details To explore sensor data for an individual trawl survey fishing event
 #' @param fishing_event_id A vector of fishing events to filter for
 #' @param sensor_name A character vector of sensor names to filter for.
 #'
@@ -80,14 +99,16 @@ get_sensor_data_trawl_fe <- function(fishing_event_id = NULL,
   names(.d) <- tolower(names(.d))
   .d$attribute <- tolower(.d$attribute)
   .d$attribute <- gsub("dissolved oxygen", "do", .d$attribute)
-  .d <- unique(.d)
+  .d <- dplyr::distinct(.d)
   .d <- .d %>% mutate(attribute = paste0(attribute, "_", unit)) %>%
     select(-unit)
 }
 
-
-#' Environmental data extraction for sensors deployed on longline survey gear
+#' @details
+#' `get_sensor_data_ll_td()`: Environmental data extraction for sensors
+#' deployed on longline survey gear.
 #'
+#' @export
 #' @rdname get_environmental_data
 get_sensor_data_ll_td <- function(ssid = NULL,
   attribute = c("temperature", "depth"),
@@ -107,7 +128,7 @@ get_sensor_data_ll_td <- function(ssid = NULL,
   .d <- run_sql("GFBioSQL", .q)
   names(.d) <- tolower(names(.d))
   .d$attribute <- tolower(.d$attribute)
-  .d <- unique(.d)
+  .d <- dplyr::distinct(.d)
 
   .d <- .d %>%
     mutate(attribute = paste0(attribute, "_", unit)) %>%
@@ -122,6 +143,12 @@ get_sensor_data_ll_td <- function(ssid = NULL,
   as_tibble(.d)
 }
 
+#' @details
+#' `get_sensor_data_ll_td_fe()`: Environmental data extraction for sensors
+#' deployed on longline survey gear for individual fishing events.
+#'
+#' @export
+#' @rdname get_environmental_data
 get_sensor_data_ll_td_fe <- function(fishing_event_id = NULL,
   attribute = c("temperature", "depth")) {
   .q <- read_sql("get-sensor-data-ll-td-fe.sql")
@@ -141,15 +168,17 @@ get_sensor_data_ll_td_fe <- function(fishing_event_id = NULL,
   names(.d) <- tolower(names(.d))
   .d$attribute <- tolower(.d$attribute)
   .d$attribute <- gsub("dissolved oxygen", "do", .d$attribute)
-  .d <- unique(.d)
+  .d <- dplyr::distinct(.d)
   .d <- .d %>% mutate(attribute = paste0(attribute, "_", unit)) %>%
     select(-unit)
 }
 
-#' Environmental data extraction for ctd's deployed on longline surveys.
-#' Ctd deployments are not tied directly to longline survey fishing event id's.
-#' The unique fishing event id's from the deployments can be linked to longline
-#' survey data by joining on the block designation (site number).
+#' @details
+#' `get_sensor_data_ll_ctd()`: Environmental data extraction for ctd's deployed
+#' on longline surveys. Ctd deployments are not tied directly to longline survey
+#' fishing event id's. The unique fishing event id's from the deployments can be
+#' linked to longline survey data by joining on the block designation (site
+#' number).
 #'
 #' @export
 #' @rdname get_environmental_data
@@ -171,7 +200,7 @@ get_sensor_data_ll_ctd <- function(ssid = NULL,
   .d <- run_sql("GFBioSQL", .q)
   names(.d) <- tolower(names(.d))
   .d$attribute <- tolower(.d$attribute)
-  .d <- unique(.d)
+  .d <- dplyr::distinct(.d)
 
   .d <- .d %>%
     mutate(attribute = paste0(attribute, "_", unit)) %>%
@@ -186,8 +215,9 @@ get_sensor_data_ll_ctd <- function(ssid = NULL,
   as_tibble(.d)
 }
 
-#' Envirnmental data extraction (ctd data) near longline survey sites
-#' for individual fishing events
+#' @details
+#' `get_sensor_data_ll_ctd_fe()`: Envirnmental data extraction (ctd data) near
+#' longline survey sites for individual fishing events.
 #'
 #' @export
 #' @rdname get_environmental_data
@@ -210,7 +240,7 @@ get_sensor_data_ll_ctd_fe <- function(fishing_event_id = NULL,
   names(.d) <- tolower(names(.d))
   .d$attribute <- tolower(.d$attribute)
   .d$attribute <- gsub("dissolved oxygen", "do", .d$attribute)
-  .d <- unique(.d)
+  .d <- dplyr::distinct(.d)
   .d <- .d %>% mutate(attribute = paste0(attribute, "_", unit)) %>%
     select(-unit)
 }
