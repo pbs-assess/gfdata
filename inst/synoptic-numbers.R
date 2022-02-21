0b08428c8cda044fb3c4b2c84eb25f586a1743e0
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2022-02-21 10:33:37 -0800

    function for catch weight and sample weight checks

--- inst/synoptic-numbers.R
+++ inst/synoptic-numbers.R
@@ -47,8 +47,8 @@ predict_weight <- function(xx) {
     stop("All weights are included in your dataset, no need to predict weights.", call. = FALSE)
   }
 
-  #2 . check sex codes are correct
-  x <- c(0,1,2)
+  # 2 . check sex codes are correct
+  x <- c(0, 1, 2)
   if (!any(unique(xx$sex) %in% x)) {
     stop("codes do not match 0,1,2", call. = FALSE)
   }
@@ -74,7 +74,7 @@ predict_weight <- function(xx) {
 
     trawl_m <- filter(xx, sex == 0)
     trawl_m$weight_predicted_m <- exp(maletw$pars$log_a +
-                                      maletw$pars$b * log(trawl_m$length)) * 1000
+      maletw$pars$b * log(trawl_m$length)) * 1000
 
     xxx <- filter(xx, sex == 2 | sex == 0)
     xxx$sex <- replace(xxx$sex, xxx$sex == 0, 2) # females are 2
@@ -92,12 +92,12 @@ predict_weight <- function(xx) {
 
     trawl_f <- filter(xx, sex == 0)
     trawl_f$weight_predicted_f <- exp(femaletw$pars$log_a +
-                                      femaletw$pars$b * log(trawl_f$length)) * 1000
+      femaletw$pars$b * log(trawl_f$length)) * 1000
 
-    predicted_weight_tw <- inner_join(trawl_m,trawl_f)
-    predicted_weight_tw <- predicted_weight_tw %>% mutate(weight_predicted = rowSums(across(weight_predicted_f:weight_predicted_f))/2)
+    predicted_weight_tw <- inner_join(trawl_m, trawl_f)
+    predicted_weight_tw <- predicted_weight_tw %>% mutate(weight_predicted = rowSums(across(weight_predicted_f:weight_predicted_f)) / 2)
     predicted_weight_nosex <- select(predicted_weight_tw, year, survey_abbrev, fishing_event_id, survey_id, species_code, sample_id, sex, length, weight, specimen_id, weight_predicted)
-       }
+  }
 
   # check if there are codes for male and female, if only one sex then run one, otherwise run both.
   x <- c(1, 2)
@@ -139,8 +139,7 @@ predict_weight <- function(xx) {
 
     predicted_weight_tw <- rbind(trawl_m, trawl_f)
     predicted_weight_tw2 <- select(predicted_weight_tw, year, survey_abbrev, fishing_event_id, survey_id, species_code, sample_id, sex, length, weight, specimen_id, weight_predicted)
-
-    }
+  }
   predicted_weight_tw3 <- rbind(predicted_weight_nosex, predicted_weight_tw2)
   return(predicted_weight_tw3)
 }
@@ -197,7 +196,7 @@ get_nfish <- function(x, too_small = 0.04, too_big = 12) {
     # is tot_samp_weight reasonable? test against catch_weight:
     if (tot_samp_weight / catch_weight > 1.1) {
       warning("total sample weight / catch weight is too big.")
-      print(paste("Check fishing_event_id number:",as.character(x$fishing_event_id)))
+      print(paste("Check fishing_event_id number:", as.character(x$fishing_event_id)))
       est_catch_count <- NA
       method <- "total sample weight / catch weight is too big"
     } # sample weight looks plausible, proceed
@@ -218,43 +217,19 @@ get_nfish <- function(x, too_small = 0.04, too_big = 12) {
 ##############################################################################################
 # CODE TO RUN ABOVE FUNCTIONS ON THE SAMPLE DATABASE
 ##############################################################################################
-#predict weights
+
+# predict weights
 samps_weightcomplete <- predict_weight(samps)
 
-#create a weight column that has either predicted and collected weights
+# create a weight column that has either predicted and collected weights
 samps_weightcomplete$weight_complete <- ifelse(!is.na(samps_weightcomplete$weight) == TRUE, samps_weightcomplete$weight, samps_weightcomplete$weight_predicted)
 
 # join set and samples with predicted weight
 y2 <- left_join(sets, select(samps_weightcomplete, year, survey_abbrev, survey_id, species_code, sample_id, length, weight_complete, specimen_id, fishing_event_id),
-                by = c("year" = "year", "survey_abbrev" = "survey_abbrev", "fishing_event_id" = "fishing_event_id"))
+  by = c("year" = "year", "survey_abbrev" = "survey_abbrev", "fishing_event_id" = "fishing_event_id")
+)
 dim(y2)
 
-## get rid of sets with no samples
-unique(y2$survey_abbrev)
-names(y2)
-
-#plot of samp weights
-y3 <- y2 %>% group_by(fishing_event_id) %>% summarize(sample_sum)
-ggplot(y2, aes(catch_weight, weight_complete/1000)) + #shows very high catch_weights
-  geom_point() +
-  scale_color_viridis_c(trans = "log10") +
-  #scale_x_log10() +
-  scale_y_log10() +
-  geom_text(aes(label = as.character(fishing_event_id)))
-  #facet_wrap(~survey_abbrev)
-
-ggplot(y2, aes(catch_weight)) + #shows very high catch_weights
-  geom_density() +
-  scale_x_log10() #+
-test <- filter(y2, fishing_event_id == 3233304)
-
-ggplot(y2, aes(weight_complete/1000)) + #shows very high catch_weights
-  geom_density() +
-  scale_x_log10() #+
-#  facet_wrap(~survey_abbrev)
-
-
-#
 yy <- group_by(y2, survey_abbrev, fishing_event_id) %>%
   mutate(any_weight_na = any(is.na(weight_complete))) %>%
   filter(!any_weight_na)
@@ -274,11 +249,11 @@ counts <- yy %>%
   group_split() %>%
   purrr::map_dfr(get_nfish)
 unique(counts$method)
-filter(counts, fishing_event_id ==481885)
+filter(counts, fishing_event_id == 481885)
 
-#problems
-#look at some of the entires that had warnings
-ch <- filter(y2, fishing_event_id == 2179105 )
+# problems
+# look at some of the entires that had warnings
+ch <- filter(y2, fishing_event_id == 2179105)
 x <- filter(y, fishing_event_id == 1281290)
 sum(ch$weight_complete) / 1000
 ch$catch_weight
@@ -289,12 +264,11 @@ unname(quantile(counts$count, probs = 0.9)) * 10 # upper quantile number of coun
 highcounts <- counts %>% filter(count > 1000)
 
 highcounts <- counts %>% filter(count > 500)
-highcount_fi <- filter(y2 , fishing_event_id ==501905) #QSC syn has a catch_weight of 12465.4 kg and a count of 13497.5669 fish
+highcount_fi <- filter(y2, fishing_event_id == 501905) # QSC syn has a catch_weight of 12465.4 kg and a count of 13497.5669 fish
 hist(counts$count)
-#fishing_event_id == 3233304 has an implied weight of 0.0008913684 per fish and counts of 4,150 fish
-#fishing_event_id == 1281273 has an implied weight of 1.1117957325 per fish and counts of 9,941.5744 fish
-#fishing_event_id == 4546331 has an implied weight of 1.0810000000 per fish and counts of 8,978.0759 fish
-
+# fishing_event_id == 3233304 has an implied weight of 0.0008913684 per fish and counts of 4,150 fish
+# fishing_event_id == 1281273 has an implied weight of 1.1117957325 per fish and counts of 9,941.5744 fish
+# fishing_event_id == 4546331 has an implied weight of 1.0810000000 per fish and counts of 8,978.0759 fish
 
 counts2 <- filter(counts, count < 500)
 hist(counts2$implied_weight_per_fish)
@@ -317,62 +291,129 @@ ggplot(yy2, aes(calculated_count, catch_weight, colour = implied_weight_per_fish
   facet_wrap(~survey_abbrev)
 
 
-
-#####looking for problems and resolving
-#plot of samp weights
-y3 <- y2 %>% group_by(fishing_event_id) %>% summarize(sample_sum)
-ggplot(y2, aes(catch_weight, weight_complete/1000)) + #shows very high catch_weights
-  geom_point() +
-  scale_color_viridis_c(trans = "log10") +
-  scale_x_log10() +
-  scale_y_log10() +
-  geom_text(aes(label = as.character(fishing_event_id)))
-#facet_wrap(~survey_abbrev)
-
-df <- select(y2, catch_weight, weight_complete)
-#create new column in data frame to hold Mahalanobis distances
-df2 <- df %>% drop_na() %>% filter(catch_weight > 0 & weight_complete>0) %>% mutate(logcatch_weight = log10(catch_weight), logweight_complete = log10(weight_complete)) %>%
-  select(logcatch_weight, logweight_complete)
-df2$mahal <- mahalanobis(df2, colMeans(df2, na.rm = TRUE), cov(df2))
-
-#create new column in data frame to hold p-value for each Mahalanobis distance
-df2$p <- pchisq(df2$mahal, df=3, lower.tail=FALSE)
-plot(df2$logcatch_weight, df2$logweight_complete)
-df3 <- filter(df2, p < 0.001)
-points(df3$logcatch_weight, df3$logweight_complete, col = 'red')
-
-#outlier catch weights
-below_2sd <- function(z) {
-  x <- log(z$catch_weight)
+#############################
+# end of run
+##############################
+below_3sd_catchweight <- function(z) {
+  x <- log10(z$catch_weight)
   x[is.infinite(x)] <- NA
-  x <- exp(sd(x, na.rm = TRUE))
-  z$within_2sd <- abs(exp(log(z$catch_weight)-mean(log(z$catch_weight), na.rm = TRUE))) < 3*x
- return (z)
-}
-
-p <- y2 %>% below_2sd()
-#x2 <- y2 %>% group_by(fishing_event_id) %>% below_2sd()
-x2 <- filter(p, within_2sd == FALSE)
-length(unique(x2$survey_desc)) #6 surveys with catch_weights outside of the 99% CI
-plot(x$catch_weight, x$weight_complete )
-x2 <- x %>% filter(within_2sd == FALSE)
-points(x2$catch_weight,x2$weight_complete,  col = 'red')
-
-
-#outlier reported sample weights
-test <- filter(y2, fishing_event_id == 3233304)
-z <- test
-
-below_2sd <- function(z) {
-  x <- exp(sd(log(z$weight_complete), na.rm = TRUE))
-  z$within_2sd <- abs(exp(log(z$weight_complete)-mean(log(z$weight_complete), na.rm = TRUE))) < 3*x
+  x1 <- exp(sd(x, na.rm = TRUE))
+  mean <- mean(x, na.rm = TRUE)
+  z$within_3sd <- abs(exp(log10(z$catch_weight) - mean)) < 3 * x1
   return(z)
 }
 
-x <- y2 %>% below_2sd()
-#x2 <- y2 %>% group_by(fishing_event_id) %>% below_2sd()
-x2 <- filter(x, within_2sd == FALSE)
-length(unique(x2$survey_desc)) #6 surveys with catch_weights outside of the 99% CI
-plot(x$weight_complete, x$catch_weight)
-x2 <- x %>% filter(within_2sd == FALSE)
-points(x2$weight_complete, x2$catch_weight, col = 'red')
+below_3sd <- function(z) {
+  x <- exp(sd(log(z$weight_complete), na.rm = TRUE))
+  z$within_3sd <- abs(exp(log(z$weight_complete) - mean(log(z$weight_complete), na.rm = TRUE))) < 3 * x
+  return(z)
+}
+
+
+### write a function that completes checks on the database
+y2 # has sets with no sampes
+
+x <- set_sample_check(y2)
+
+set_sample_check <- function(y2) {
+
+  stopifnot(length(unique(y2$species_common_name)) == 1L)
+  # stopifnot(length(unique(x$catch_weight)) == 1L)
+
+  # 1. check for sets with no samples
+  nosamps <- y2 %>%
+    group_by(survey_abbrev, fishing_event_id) %>%
+    summarize(na_in_sample_id = sum(sample_id)) %>%
+    filter(is.na(na_in_sample_id) == TRUE)
+  tibble(fishing_event_id = nosamps$fishing_event_id, report = "no samples for this fishing event id")
+
+  # 2. check for sets where sum sample weight is greater than catch_weight
+  #  ##### multivariate outliers catch weight and sum of weight of samples
+  # calculate where sum samples are > weight_complete
+  # plot of samp weights
+  higher_sampsweight <- y2 %>%
+    group_by(fishing_event_id, catch_weight) %>%
+    summarize(sample_sum = sum(weight_complete) / 1000)
+
+  higher_sampsweight2 <- higher_sampsweight %>% filter(sample_sum > catch_weight) %>% distinct(fishing_event_id)
+
+  df <- tibble(fishing_event_id = higher_sampsweight2$fishing_event_id, report = "sample weight is > catch weight")
+
+  plot_sample_bigger_catch <-
+    ggplot(y3, aes(catch_weight, sample_sum), colour = "grey50") +
+    geom_point() +
+    scale_color_viridis_c(trans = "log10") # +
+  # geom_text(aes(label = as.character(fishing_event_id)))
+  diag <- plot_sample_bigger_catch +
+    geom_point(data = y4, mapping = aes(catch_weight, sample_sum), col = "red") +
+    scale_x_log10() +
+    scale_y_log10()
+
+
+  # create new column in data frame to hold Mahalanobis distances
+  multi_outliers <- higher_sampsweight %>%
+    select(catch_weight, sample_sum, fishing_event_id) %>%
+    drop_na() %>%
+    filter(catch_weight > 0 & sample_sum > 0) %>%
+    mutate(
+      logcatch_weight = log10(catch_weight),
+      logsample_sum = log10(sample_sum)
+    ) %>%
+    select(logcatch_weight, logsample_sum, fishing_event_id)
+
+
+  multi_outliers$mahal <- mahalanobis(multi_outliers, colMeans(multi_outliers, na.rm = TRUE), cov(multi_outliers))
+  multi_outliers$p <- pchisq(multi_outliers$mahal, df = 3, lower.tail = FALSE)
+
+  multi_outliers2 <- filter(multi_outliers, p < 0.001)
+  multi_outliers3 <- filter(multi_outliers, p < 0.001) %>% distinct()
+
+  x <- ggplot(multi_outliers, aes(logcatch_weight, logsample_sum / 1000)) + # shows very high catch_weights
+    geom_point() +
+    scale_color_viridis_c(trans = "log10") #+
+  #  geom_text(aes(label = as.character(fishing_event_id)))
+
+  diag2 <- x + geom_point(multi_outliers2, mapping = aes(logcatch_weight, logsample_sum / 1000), colour = "red")
+
+  df <- rbind(df, tibble(fishing_event_id = multi_outliers3$fishing_event_id,
+                         report = "mutivariate outlier between catch weight and sample weight (mahalanobis distance"))
+
+
+  # 4. find catch_weights that fall outside 3 SD
+  catch_weight_SD <- y2 %>%
+    distinct(fishing_event_id, .keep_all = TRUE) %>%
+    below_3sd_catchweight()
+  catch_weight_SD2 <- filter(catch_weight_SD, within_3sd == FALSE)
+  length(unique(catch_weight_SD2$survey_desc)) # 38 surveys with catch_weights outside of the 99% CI
+
+  df <- rbind(df, tibble(fishing_event_id = catch_weight_SD2$fishing_event_id,
+                         report = "catch_weight falls outside of 3 SD of all catch weights"))
+
+  catch_weight_3sd <- ggplot(catch_weight_SD, aes(weight_complete / 1000, catch_weight)) + # shows very high catch_weights
+    geom_point() +
+    # scale_x_log10() +
+    # scale_y_log10() +
+    scale_color_viridis_c(trans = "log10") #+
+  #  geom_text(aes(label = as.character(fishing_event_id)))
+
+  diag3 <- catch_weight_3sd + geom_point(catch_weight_SD2, mapping = aes(weight_complete / 1000, catch_weight), colour = "red")
+
+
+  # 5. find sample weight values outside of 3 SD
+  sample_weight_SD <- y2 %>% below_3sd()
+  # x2 <- y2 %>% group_by(fishing_event_id) %>% below_2sd()
+  sample_weight_SD2 <- filter(sample_weight_SD, within_3sd == FALSE)
+  # length(unique(x2$survey_desc)) # 6 surveys with catch_weights outside of the 99% CI
+
+  sample_weight_3SDplot <- ggplot(sample_weight_SD, aes(weight_complete / 1000, catch_weight)) + # shows very high catch_weights
+    geom_point() +
+    scale_color_viridis_c(trans = "log10") #+
+  #  geom_text(aes(label = as.character(fishing_event_id)))
+
+  diag4 <- sample_weight_3SDplot + geom_point(sample_weight_SD2, mapping = aes(weight_complete / 1000, catch_weight), colour = "red")
+
+  df <- rbind(df, tibble(fishing_event_id = sample_weight_SD2$fishing_event_id,
+                         report = "sum of sample weight per fishing id falls outside of 3 SD of all summed sample weights"))
+
+  return(df)
+}
