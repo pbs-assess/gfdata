39229b70e968481196bb22025f5bccc687f5afd0
English <Philina.English@dfo-mpo.gc.ca>
2023-08-03 11:49:46 -0700

    working hook sql

--- inst/sql/get-ll-hook-data2.sql
+++ inst/sql/get-ll-hook-data2.sql
@@ -6,11 +6,13 @@
     S.SURVEY_SERIES_ID,
     YEAR(T.TRIP_START_DATE) AS TRIP_YEAR,
       CAST(MAJOR_STAT_AREA_CODE AS SMALLINT) AS major,
-      Nall count_all_species,
-      Nb count_bait_only,
-      Ne count_empty_hooks,
-      Nbr count_bent_broken,
-      CASE MAJOR_STAT_AREA_CODE WHEN '01' THEN '4B' WHEN '03' THEN '3C'
+    Nall count_all_animals,
+    Nsp count_all_species,
+    Nb count_bait_only,
+    Ne count_empty_hooks,
+    Nnf count_not_fishing,
+    Nbr count_bent_broken,
+    CASE MAJOR_STAT_AREA_CODE WHEN '01' THEN '4B' WHEN '03' THEN '3C'
          WHEN '04' THEN '3D' WHEN '05' THEN '5A/B' WHEN '06' THEN '5A/B'
          WHEN '07' THEN '5C/D/E' WHEN '08' THEN '5C/D/E' WHEN '09' THEN '5C/D/E' ELSE NULL END AS areagrp
    FROM SURVEY S
@@ -24,20 +26,23 @@
             SELECT TRIP_ID,
                VESSEL_ID,
                FE_MAJOR_LEVEL_ID,
-               SUM(NT) AS NT,
-               SUM(NNT) AS NNT,
+               SUM(Nall) AS Nall,
+               SUM(Nsp) AS Nsp,
                SUM(Ne) AS Ne,
                SUM(Nb) AS Nb,
+               SUM(Nnf) AS Nnf,
                SUM(Nbr) AS Nbr
             FROM (
                SELECT T.TRIP_ID,
                   T.VESSEL_ID,
                   FE.FE_MAJOR_LEVEL_ID,
                   FE.FISHING_EVENT_ID,
-                  SUM(CASE SPECIES_CODE IS NOT NULL THEN 1 ELSE 0 END) AS Nall, -- all species recorded
+                  SUM(CASE WHEN SPECIES_CODE IS NOT NULL THEN 1 ELSE 0 END) AS Nsp, -- all species recorded
+                  SUM(CASE WHEN HOOK_YIELD_CODE IN (3,4,5,8) THEN 1 ELSE 0 END) AS Nall, -- all animals
                   SUM(CASE HOOK_YIELD_CODE WHEN 1 THEN 1 ELSE 0 END) AS Ne,
                   SUM(CASE HOOK_YIELD_CODE WHEN 2 THEN 1 WHEN 6 THEN 1 ELSE 0 END) AS Nb, -- bait only and bait skin
-                  SUM(CASE WHEN HOOK_CONDITION_CODE IN (1,2,3,7) THEN 1 ELSE 0 END) AS Nbr
+                  SUM(CASE WHEN HOOK_CONDITION_CODE IN (1,2,3,7) THEN 1 ELSE 0 END) AS Nbr,
+                  SUM(CASE HOOK_YIELD_CODE WHEN 9 THEN 1 ELSE 0 END) AS Nnf -- not fishing
                   --SUM(CASE HOOK_YIELD_CODE WHEN 2 THEN 1 ELSE 0 END) AS Nb (only 'Bait only' vs. bait only and bait skin)
                FROM SURVEY S
                   INNER JOIN TRIP_SURVEY TS ON
@@ -55,9 +60,8 @@
                   LEFT JOIN LONGLINE_SPECS LS
                   ON FE.FISHING_EVENT_ID = LS.FISHING_EVENT_ID
                WHERE FE_PARENT_EVENT_ID IS NOT NULL AND
-                  FE_MINOR_LEVEL_ID IS NOT NULL AND
+                  FE_MINOR_LEVEL_ID IS NOT NULL
                   -- insert ssid here
-                  ISNULL(LS.USABILITY_CODE,0) IN (0,1,2,6)
                GROUP BY T.TRIP_ID, T.VESSEL_ID, FE.FE_MAJOR_LEVEL_ID, FE.FISHING_EVENT_ID) T
             GROUP BY TRIP_ID, VESSEL_ID, FE_MAJOR_LEVEL_ID) C ON
       T.TRIP_ID = C.TRIP_ID AND T.VESSEL_ID = C.VESSEL_ID AND FE.FE_MAJOR_LEVEL_ID = C.FE_MAJOR_LEVEL_ID
