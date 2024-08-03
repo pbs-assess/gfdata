7c9f93008553e5546a1aa729c5dc470e74baf0eb
Rogers <Luke.Rogers@dfo-mpo.gc.ca>
2024-08-02 21:27:16 -0700

    updated with comparison_id

--- compare/R/functions-sets.R
+++ compare/R/functions-sets.R
@@ -36,12 +36,32 @@ compare_sets <- function (spp, ssid) {
           ssid = ssid[j]
         )
       )
-      # Identify extra fishing_event_id
-      n1 <- setdiff(d1$fishing_event_id, d2$fishing_event_id)
-      n2 <- setdiff(d2$fishing_event_id, d1$fishing_event_id)
-      # Identify and store extra fishing_event_id rows
+      # Reset value
+      n1 <- NULL
+      n2 <- NULL
+      # Create comparison columns
+      if (!is.null(d1)) {
+        d1 <- d1 |>
+          tidyr::drop_na(species_code, fishing_event_id) |>
+          dplyr::mutate(
+            comparison_id = paste0(species_code, fishing_event_id),
+            .before = 1
+          )
+      }
+      if (!is.null(d2)) {
+        d2 <- d2 |>
+          tidyr::drop_na(species_code, fishing_event_id) |>
+          dplyr::mutate(
+            comparison_id = paste0(species_code, fishing_event_id),
+            .before = 1
+          )
+      }
+      # Identify extra comparison_id
+      n1 <- setdiff(d1$comparison_id, d2$comparison_id)
+      n2 <- setdiff(d2$comparison_id, d1$comparison_id)
+      # Identify and store extra comparison_id rows
       if (length(n1) > 0) {
-        r1 <- which(d1$fishing_event_id %in% n1)
+        r1 <- which(d1$comparison_id %in% n1)
         # Bind rows - Should accept different sets of columns
         s1 <- dplyr::bind_rows(
           s1,
@@ -49,7 +69,7 @@ compare_sets <- function (spp, ssid) {
         )
       }
       if (length(n2 > 0)) {
-        r2 <- which(d2$fishing_event_id %in% n2)
+        r2 <- which(d2$comparison_id %in% n2)
         # Bind rows - Should accept different sets of columns
         s2 <- dplyr::bind_rows(
           s2,
@@ -60,7 +80,7 @@ compare_sets <- function (spp, ssid) {
   }
 
   # Remove NAs (known issue)
-  # s1 <- s1 |> tidyr::drop_na(fishing_event_id)
+  # s1 <- s1 |> tidyr::drop_na(comparison_id)
 
   # Return
   list(e1 = e1, e2 = e2, s1 = s1, s2 = s2)
@@ -85,6 +105,17 @@ compare_set_values <- function (spp, ssid) {
       try(d2 <- gfdata::get_survey_sets2(species = spp[i], ssid = ssid[j]))
       # Only compares non-null output for both functions
       if (!is.null(d1) & !is.null(d2)) {
+        # Create comparison columns
+        d1 <- d1 |>
+          dplyr::mutate(
+            comparison_id = paste0(species_code, fishing_event_id),
+            .before = 1
+          )
+        d2 <- d2 |>
+          dplyr::mutate(
+            comparison_id = paste0(species_code, fishing_event_id),
+            .before = 1
+          )
         # Only compare shared colnames
         cn <- intersect(colnames(d1), colnames(d2))
         # Shared columns
@@ -93,14 +124,14 @@ compare_set_values <- function (spp, ssid) {
         # Bind rows
         dd <- dplyr::bind_rows(d1, d2) |>
           # Drop NAs
-          tidyr::drop_na(fishing_event_id) |>
+          tidyr::drop_na(species_code, fishing_event_id) |>
           # Arrange so same fishing_event_id are in sequential rows
-          dplyr::arrange(fishing_event_id, fn) |>
+          dplyr::arrange(species_code, fishing_event_id, fn) |>
           # Remove small differences in density (<< 1e-03)
           dplyr::mutate(density_ppkm2 = round(density_ppkm2, 3)) |>
           # Collapse to one row if equal (except in fn column)
           dplyr::distinct(dplyr::across(-fn), .keep_all = TRUE) |>
-          dplyr::group_by(fishing_event_id) |>
+          dplyr::group_by(comparison_id) |>
           # Keep only groups with more than one row (the inconsistent groups)
           dplyr::filter(n() > 1) |>
           dplyr::ungroup()
