7a17138470e675af7d6d9aeda8b68a471027709a
ecophilina <paenglis@lakeheadu.ca>
2024-08-07 15:26:17 -0700

    rename to get_all_survey_sets; add activity matches and remove duplicates option; remove some unused options

--- R/get-survey-sets.R
+++ R/get-survey-sets.R
@@ -1,16 +1,14 @@
 #' Get survey set data faster and more comprehensively
 #'
 #' @param years If NULL, returns all years.
-#' @param join_sample_ids If `TRUE` then the sample IDs will be joined in. This
-#'   may result in repeated rows of data if the same sample ID is part of
-#'   different survey stratifications.
-#' @param verbose If `TRUE` then extra messages were reprinted during data
-#'   extraction. Useful to monitor progress.
+#' @param join_sample_ids This option was problematic, so now reverts to FALSE.
+#' @param verbose Doesn't do anything in this version.
 #' @param remove_false_zeros If `TRUE` will make sure weights > 0 don't have
 #'   associated counts of 0 and vice versa. Only applies to trawl data where
 #'   counts are only taken for small catches.
-#' @param sleep System sleep in seconds between each survey-year
-#'   to be kind to the server.
+#' @param remove_duplicates Remove duplicated event records due to overlapping survey
+#'   stratifications when original_ind = 'N', or from known issues with MSSM trips including both survey areas.
+#' @param include_activity_matches Get all surveys with activity codes that match chosen ssids.
 #' @export
 #' @rdname get_data
 #' @examples
@@ -29,11 +27,13 @@
 #' ## `time_retrieved`.
 #' }
 #'
-get_survey_sets2 <- function(species,
+get_all_survey_sets <- function(species,
                              ssid = c(1, 3, 4, 16, 2, 14, 22, 35, 36, 39, 40),
                              years = NULL,
                              join_sample_ids = FALSE, verbose = FALSE,
                              remove_false_zeros = FALSE,
+                             remove_duplicates = FALSE,
+                             include_activity_matches = FALSE,
                              usability = c(0, 1, 2, 6)) {
 
   .q <- read_sql("get-survey-sets.sql")
@@ -42,14 +42,28 @@ get_survey_sets2 <- function(species,
     .q <- inject_filter("AND SP.SPECIES_CODE IN", species, sql_code = .q)
   }
 
+
   if (!is.null(ssid)) {
+    if (include_activity_matches){
+      ## draft approach that gets all samples collected using the same activities as the ssid(s) of interest
+      .a <- read_sql("get-activity-code.sql")
+      .a <- run_sql("GFBioSQL", .a)
+
+      .a <-  filter(.a, SURVEY_SERIES_ID %in% ssid) |> distinct()
+
+      activities <- unique(.a$ACTIVITY_CODE)
+      .q <- inject_filter("AND TA.ACTIVITY_CODE IN", activities,
+                          sql_code = .q,
+                          search_flag = "-- insert ssid here", conversion_func = I
+      )
+    } else {
     survey_ids <- get_survey_ids(ssid)
     .q <- inject_filter("AND S.SURVEY_SERIES_ID IN", ssid,
       sql_code = .q,
       search_flag = "-- insert ssid here", conversion_func = I
     )
   }
-
+ }
   # if (!is.null(major)) {
 
   #   .q <- inject_filter("AND SM.MAJOR_STAT_AREA_CODE =", major, .q,
@@ -64,26 +78,26 @@ get_survey_sets2 <- function(species,
     .d <- filter(.d, YEAR %in% years)
   }
 
-
   if (join_sample_ids) {
     # give us each sample_id associated with each fishing_event_id and species:
-
-    sample_trip_ids <- get_sample_trips()
-    areas <- get_strata_areas()
-
-    .d <- left_join(.d, sample_trip_ids,
-      by = c("SPECIES_CODE", "FISHING_EVENT_ID")
-    ) %>%
-      left_join(areas, by = c("SURVEY_ID", "GROUPING_CODE"))
+    # sample_trip_ids <- get_sample_trips()
+    # areas <- get_strata_areas()
+    #
+    # .d <- left_join(.d, sample_trip_ids,
+    #   by = c("SPECIES_CODE", "FISHING_EVENT_ID")
+    # ) %>%
+    #   left_join(areas, by = c("SURVEY_ID", "GROUPING_CODE"))
 
     warning(
-      "Adding sample_id will duplicate some fishing events.",
-      "This occurs when the same species was assigned two distinct sample ids."
+      "The join_sample_ids option has been removed. To bind with ",
+      "sample data, it is safer to use the include_event_info = TRUE ",
+      "option in get_all_survey_samples() instead."
     )
   }
 
-
   # Just to pull out up to date list of ssids associated with trawl/ll gear type.
+  # Sys.sleep(0.05) # might be useful if server has difficulty
+
   trawl <- run_sql("GFBioSQL", "SELECT
     S.SURVEY_SERIES_ID
     FROM SURVEY_SERIES SS
@@ -98,6 +112,8 @@ get_survey_sets2 <- function(species,
     TRAWL_IND, GEAR_CODE
     ORDER BY S.SURVEY_SERIES_ID")
   trawl <- unique(trawl$SURVEY_SERIES_ID)
+
+  # Sys.sleep(0.05)
 
   ll <- run_sql("GFBioSQL", "SELECT
     S.SURVEY_SERIES_ID
@@ -119,36 +135,103 @@ get_survey_sets2 <- function(species,
     stop("No survey set data for selected species.")
   }
 
-
   names(.d) <- tolower(names(.d))
 
+
+  # whenever ssid is included, but not activity matches
+  # we need to drop duplicated records from trips that include multiple surveys
+  if (!include_activity_matches & !is.null(ssid)) {
+    .d <- filter(.d, (survey_series_id %in% c(ssid)))
+  }
+
+  # if using include_activity_matches = TRU`then remove_duplicates = TRUE
+  if (include_activity_matches & !is.null(ssid)) {
+    remove_duplicates <- TRUE
+  }
+
+
+  # check if there are duplicate fishing_event ids
+  if (length(.d$fishing_event_id) > length(unique(.d$fishing_event_id))) {
+
+    if (remove_duplicates) {
+      # add custom fixes for problem surveys here:
+      # first split data into unique fishing_events (dd1) and ones with duplicates (dd2)
+
+      .dd <- .d[duplicated(.d$fishing_event_id),]
+      dd1 <- filter(.d, !(fishing_event_id %in% c(unique(.dd$fishing_event_id))))
+      dd2 <- filter(.d, (fishing_event_id %in% c(unique(.dd$fishing_event_id))))
+
+      # then only applying fixes to duplicated fishing_events in case some are miss-assigned but not duplicated cases
+      # for shrimp survey sets in both qcs and wcvi that were done on the same trip they get duplicated by the sql call
+      # note: getting some that violate these rules but aren't duplicated... eg. fe 1720260, 1720263
+      dd2 <- dd2[which(!(dd2$survey_series_id == 6 & dd2$major_stat_area_code %in% c("03", "04"))),]
+      dd2 <- dd2[which(!(dd2$survey_series_id == 7 & dd2$major_stat_area_code %in% c("05", "06"))),]
+
+      # for sablefish sets with inlets and offshore on same trip ???
+      # dd2 <- dd2[which(!(dd2$survey_series_id == ## & dd2$reason %in% c(""))),]
+      # dd2 <- dd2[which(!(dd2$survey_series_id == ## & dd2$reason %in% c(""))),]
+
+      # for dogfish and HBLL sets on same trip ???
+
+      .d <- bind_rows(dd1, dd2)
+
+      # if so, separate original_ind from not
+      .dy <- filter(.d, original_ind == "Y")
+      .dn <- filter(.d, original_ind != "Y")
+
+      # and only keep those not original_ind = Y when the fishing_event id was missing
+      .d <- bind_rows(.dy, filter(.dn, !(fishing_event_id %in% c(unique(.dy$fishing_event_id)))))
+
+      # check if there are still duplicated fishing_event ids
+      if (length(.d$fishing_event_id) > length(unique(.d$fishing_event_id))) {
+        warning(
+          "Duplicate fishing_event IDs are still present despite ",
+          "`remove_duplicates = TRUE`. This is potentially because of ",
+          "multiple samples from the same event (), overlapping survey ",
+          "stratifications. If working with the data yourself, you should ",
+          "filter them after selecting specific survey stratifications. ",
+          "For example, `dat <- dat[!duplicated(dat$fishing_event_id), ]`. ",
+          "Tidying and plotting functions within gfplot will do this for you."
+        )
+      }
+
+    } else {
+
+      # check if there are duplicated fishing_event ids (this often true for SABLE and MSSM surveys)
+      if (length(.d$fishing_event_id) > length(unique(.d$fishing_event_id))) {
+        warning(
+          "Duplicate fishing_event IDs are present. This is usually because of multiple ",
+          "samples from the same fishing_event, overlapping survey stratifications, ",
+          "or trips that include more than one type of survey. Some cases of the ",
+          "latter two case can be resolved by setting 'remove_duplicates = TRUE'. ",
+          "If working with the data yourself, filter them after selecting specific ",
+          "surveys. For example, `dat <- dat[!duplicated(dat$fishing_event_id), ]`. ",
+          "The tidying and plotting functions within gfplot will do this for you."
+        )
+      }
+    }
+  }
 
   # get all fishing event info
   .fe <- read_sql("get-event-data.sql")
 
-  if (!is.null(ssid)) {
+  # get only events from surveys that have recorded any of the species selected
+  .d <- filter(.d, catch_count > 0|catch_weight > 0) # shouldn't be needed but there were some
+  ssid <- unique(.d$survey_series_id)
 
-    # survey_ids <- get_survey_ids(ssid)
-    .fe <- inject_filter("AND S.SURVEY_SERIES_ID IN", ssid,
-      sql_code = .fe,
-      search_flag = "-- insert ssid here", conversion_func = I
-    )
-  } else {
-    # if ssid is NULL, get only events from surveys that have recorded any of the species selected
-    .d <- filter(.d, catch_count > 0|catch_weight > 0) # shouldn't be needed but there were some
+  .fe <- inject_filter("AND S.SURVEY_SERIES_ID IN", ssid,
+    sql_code = .fe,
+    search_flag = "-- insert ssid here", conversion_func = I
+  )
 
-    ssid <- unique(.d$survey_series_id)
-    .fe <- inject_filter("AND S.SURVEY_SERIES_ID IN", ssid,
-      sql_code = .fe,
-      search_flag = "-- insert ssid here", conversion_func = I
-    )
-    # TODO: something to address replicated events across different survey series
-    warning(
-      "Returning all survey series that contain any of the species saught.",
-      "Probably advisable to call either just one species, or just one survey type at a time.",
-      "If not using default ssids, note that some survey series are nested within eachother.",
-      "This can result in the duplication of fishing events in the dataframe."
-    )
+  if (is.null(ssid)) {
+  # something to address replicated events across different survey series
+  warning(
+    "Returning all survey series that recorded any of the species saught.",
+    "Probably advisable to call either just one species, or just one survey type at a time.",
+    "If not using default ssids, note that some survey series are nested within eachother.",
+    "This can result in the duplication of fishing events in the dataframe."
+  )
   }
 
   fe <- run_sql("GFBioSQL", .fe)
@@ -179,7 +262,9 @@ get_survey_sets2 <- function(species,
         -fe_minor_level_id,
         -fe_sub_level_id,
         -hook_code,
-        -lglsp_hook_count
+        -lglsp_hook_count,
+        -hook_desc,
+        -hooksize_desc
       ))
     ) %>% left_join(.d)
   } else {
@@ -326,7 +411,7 @@ get_survey_sets2 <- function(species,
   missing_species <- setdiff(species_codes, .d$species_code)
   if (length(missing_species) > 0) {
     warning(
-      "The following species codes do not have survey set data in GFBio.",
+      "The following species codes are not supported or do not have survey set data in GFBio.",
       paste(missing_species, collapse = ", ")
     )
   }
