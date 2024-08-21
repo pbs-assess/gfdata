f8a88de885e80b267953d867f3cfcd0188b9136f
ecophilina <paenglis@lakeheadu.ca>
2024-08-20 18:31:33 -0700

    more tests

--- compare/test-major-area-sets.R
+++ compare/test-major-area-sets.R
@@ -1,68 +1,96 @@
 ### test major areas filter on survey sets
 
-spp <- "North Pacific Spiny Dogfish"
 
+### Species --------------------------------------------------------------------
+spp <- "North Pacific Spiny Dogfish"
 spp <- "Yelloweye Rockfish"
 
+### Surveys --------------------------------------------------------------------
+
+ssid <- NULL
+# ssid <- c(39, 40, 48, 93, 76, 92) # Inside dogfish
+# ssid <- c(6,7) # MSSM
+# ssid <- c(35) # SABLE
+# ssid <- c(14) # IPHC
+
+# ssid <- c(39) # HBLL 40, 76 all
+# Error in if (any(fe$FE_SUB_LEVEL_ID > 1)) { :
+# missing value where TRUE/FALSE needed
+
+
+### MAJOR AREAS ----------------------------------------------------------------
+major_areas <- NULL
 
 ## all canadian waters and unknown
-major_areas <- c("0","00","01", "02", "03", "04", "05", "06", "07", "08", "09", "11",
-                 "71","72","73","74","75","76","77","99")
+major_areas <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "11",
+                 "71","72","73","74","75","76","77","99") # unknown "0","00",
+
+# major_areas <- c("03","04") # WCVI
+# major_areas <- c("01","68") # SOG and Puget Sound
 
 
-d <-get_all_survey_samples(species = spp, ssid = NULL, major = major_areas, remove_duplicates = TRUE)
-
-
-x <- d# |> filter(is.na(skate_count)|skate_count == 1)
-dd <- x[duplicated(x$specimen_id),]
-dd1 <- filter(x, (specimen_id %in% c(dd$specimen_id)))
-
-
-
-
-
-
-major_areas <- c("01", "68") # SOG and Puget Sound
-
-d <-get_all_survey_sets(species = spp, ssid = NULL, major = major_areas)
-
-x <- d# |> filter(is.na(skate_count)|skate_count == 1)
-
-dd <- x[duplicated(x$fishing_event_id),]
-dd <- x[duplicated(x$skate_id),]
-
-dd2 <- filter(x, (fishing_event_id %in% c(dd$fishing_event_id)))
-
-
-xx <- d1 |> group_by(survey_series_id, survey_series_desc,
-                    # major_stat_area_code,
-                    # minor_stat_area_code,
-                    original_ind) |>
-  summarise(n = n())
-
-
-d2 <- get_all_survey_sets(species = spp,
-                          ssid = c(35, 41, 42, 43),
-                          major = NULL,
+### Sets -----------------------------------------------------------------------
+d <- get_all_survey_sets(species = spp,
+                          ssid = ssid,
+                          major = major_areas,
+                          include_activity_matches = TRUE,
                           remove_duplicates = FALSE)
 
 
-x <- filter(d2, survey_series_id != 35)
+dd <- d[duplicated(d$fishing_event_id),]
+x <- d |> mutate(duplicate = ifelse(fishing_event_id %in% c(unique(dd$fishing_event_id)), "Y", "N"))
 
-dd <- x[duplicated(x$fishing_event_id),]
-
-dd2 <- filter(x, (fishing_event_id %in% c(dd$fishing_event_id)))
-
+# x <- filter(d, survey_series_id != 48)
 
 
-d <-get_all_survey_sets(species = spp, ssid = NULL, major = c("03","04"))
+dd1 <- filter(x, !(fishing_event_id %in% c(unique(dd$fishing_event_id))))
+dd2 <- filter(x, (fishing_event_id %in% c(unique(dd$fishing_event_id))))
 
-xx <- d2 |> group_by(survey_series_id, survey_series_desc,
+xx <- x  |>
+  group_by(survey_series_id, survey_series_desc, duplicate,
+           # reason_desc,
+           # major_stat_area_code,
+           # minor_stat_area_code,
+           original_ind) |>
+  summarise(n = n(), unique_fe = length(unique(fishing_event_id)))
+
+### Samples --------------------------------------------------------------------
+
+d2 <- get_all_survey_samples(species = spp,
+                          ssid = ssid,
+                          major = major_areas,
+                          unsorted_only = FALSE, random_only = FALSE,
+
+                          # include_event_info = FALSE, # TRUE causes strange duplication of ~ 18 YE samples from QCS synoptic due to missing boot_defaults
+                          remove_duplicates = TRUE)
+# d1 <- d2
+x <- d2
+dd <- x[duplicated(x$specimen_id),]
+x <- x |> mutate(duplicate = ifelse(specimen_id %in% c(unique(dd$specimen_id)), "Y", "N"))
+
+dd1 <- filter(x, !(specimen_id %in% c(dd$specimen_id)))
+dd2 <- filter(x, (specimen_id %in% c(dd$specimen_id)))
+
+xx2 <- x |> group_by(survey_series_id, survey_series_desc, duplicate,
+                     # reason_desc,
                      # major_stat_area_code,
                      # minor_stat_area_code,
                      original_ind) |>
-  summarise(n = n())
-
-d3 <- get_all_survey_sets(species = spp, ssid = c(6,7), major = NULL)
+  summarise(n = n(), unique_fe = length(unique(fishing_event_id)))
 
 
+saveRDS(d2, "f2-yelloweye-samples-all-major-for-39-ssids.rds")
+
+
+xa <- x |> filter(!is.na(age))
+
+
+xxa <- xa |>
+  group_by(survey_series_id, survey_series_desc, duplicate,
+                     # reason_desc,
+                     major_stat_area_code,
+                     # minor_stat_area_code,
+                     original_ind) |>
+  summarise(n = n(), unique_fe = length(unique(fishing_event_id)),
+            min_year = min(year),
+            max_year = max(year))
--- compare/testing-changes.R
+++ compare/testing-changes.R
@@ -14,98 +14,145 @@ source(here::here("compare", "R", "functions-samples.R"))
 # dd <- ds$x[duplicated(ds$x$fishing_event_id),]
 # # no duplicates
 
-## Inside
+# ## Inside
+# ssid <- c(
+#   45, #SOG trawl
+#   20, # synoptic vancouver region?
+#   79, # triennial vancouver region
+#   82-87, #Jig 4B
+#   13, # inside longline?
+#   15, # SOG lingcod
+#   39, 40, 48, 93, 76, 92)
+# spp <- "North Pacific Spiny Dogfish"
+#
+# ## Outside
+#
+# maj <- get_major_areas()
+# ssid <- c(0,
+#           #1,
+#           2,3,
+#           #4,
+#           5,
+#           #6,7, 8, 9, 10, 11,
+#           14,
+#           22,
+#           # 25, # thornyhead
+#           21, 26,
+#           35, 36,
+#           # 46, 48,
+#           49, 50, 51,
+#           67, 68,
+#           81, 94)
+#
 ssid <- c(
+  #OUTSIDE
+  1,
+  2,3,
+  4,
+  5,
+  6,7, 8, 9, 10, 11,
+  14,
+  22, 36, # HBLL
+  25, # thornyhead
+  21, 26,
+  35,
+  46,
+  49, 50, 51,
+  67, 68,
+  81, 94,
+  #INSIDE
   45, #SOG trawl
   20, # synoptic vancouver region?
   79, # triennial vancouver region
   82-87, #Jig 4B
   13, # inside longline?
   15, # SOG lingcod
-  39, 40, 48, 93, 76, 92)
-spp <- "North Pacific Spiny Dogfish"
+  39, 40, # HBLL
+  48, 93, 76, 92
+  )
 
-## Outside
-
-maj <- get_major_areas()
-ssid <- c(0,
-          #1,
-          2,3,
-          #4,
-          5,
-          #6,7, 8, 9, 10, 11,
-          14,
-          22,
-          # 25, # thornyhead
-          21, 26,
-          35, 36,
-          # 46, 48,
-          49, 50, 51,
-          67, 68,
-          81, 94)
-
-ssid <- c(
-  #OUTSIDE
-          1,
-          2,3,
-          4,
-          5,
-          6,7, 8, 9, 10, 11,
-          14,
-          22,
-          25, # thornyhead
-          21, 26,
-          35, 36,
-          46, 48,
-          49, 50, 51,
-          67, 68,
-          81, 94,
-          #INSIDE
-          45, #SOG trawl
-20, # synoptic vancouver region?
-79, # triennial vancouver region
-82-87, #Jig 4B
-13, # inside longline?
-15, # SOG lingcod
-39, 40, 48, 93, 76, 92
-)
-
-
-
-spp <- "North Pacific Spiny Dogfish"
-dset <- compare_survey_sets(spp = spp, ssid = c(35, 41, 42, 43))
-
-dset <- compare_survey_sets(spp = spp, ssid = c(6,7))
-
-
-spp <- c( # "Lingcod",
-         "Yelloweye Rockfish")
-
-major_areas <- c("0","00","01", "02", "03", "04", "05", "06", "07", "08", "09", "11")
-
+spp <- c("Lingcod")
+spp <- c("Yelloweye Rockfish")
+# major_areas <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "11") # unknown "0","00",
+major_areas <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "11",
+                 "71","72","73","74","75","76","77","99")
+#
+# dset <- compare_survey_sets(spp = spp, ssid <- c(39, 40, 48, 93, 76, 92))
+# dset <- compare_survey_sets(spp = spp, ssid = c(35, 41, 42, 43))
+# dset <- compare_survey_sets(spp = spp, ssid = c(6,7))
+#
+# saveRDS(dset, "test-yelloweye-outside-sets.rds")
 
 dsam <- compare_survey_samples(spp = spp, ssid = ssid, areas = major_areas)
 
-dsam <- compare_survey_samples(spp = spp, ssid = c(6,7))
+saveRDS(dsam, "test-lingcod-samples-all.rds")
+saveRDS(dsam, "test-yelloweye-samples-all-2.rds")
+# saveRDS(dsam, "test-yelloweye-samples-incomplete.rds")
 
-dsam <- compare_survey_samples(spp = spp, ssid = ssid)
 
-saveRDS(dset, "test-yelloweye-outside-sets.rds")
-saveRDS(dsam, "test-yelloweye-outside-samples-all.rds")
+#
+# ## INSIDE DOGFISH TEST
+# spp <- "North Pacific Spiny Dogfish"
+#
+# # x <-get_all_survey_samples(species = spp,
+# #                        ssid = NULL,
+# #                        major = c("01","68"), # SOG and Puget Sound,
+# #                        unsorted_only = FALSE, random_only = FALSE,
+# #                        remove_duplicates = TRUE)
+# # ssid <- unique(x$survey_series_id)
+# # [1] 85 82 15 39 40 48 50 34 51 45 68 76 86 87
+# dsam <- compare_survey_samples(spp = spp, ssid = ssid)
+#
+# saveRDS(dsam, "test-dogfish-inside-samples-all.rds")
+#
+#
+# dsam <- readRDS("test-yelloweye-samples-all.rds")
 
 x <- dsam$x
+
+x1 <- dsam$x |>
+  filter((major_stat_area_code %in% c("01","68")))
+
+x2 <- dsam$x |>
+  filter(!(major_stat_area_code %in% c("01","68")))
 
 check <- x |> filter(fn == 2)
 
 
-xx <- x |> group_by(fn, ssid, species, survey_series_desc) |>
-  summarise(n = n())
+xx1 <- x  |>
+  group_by(ssid,fn, species, survey_series_desc) |>
+  mutate(age_years = ifelse(!is.na(age), year, NA)) |>
+  summarise(n = n(),
+            lengths = length(na.omit(length)),
+            weights = length(na.omit(weight)),
+            ages = length(na.omit(age)),
+            maturities = sum(na.omit(maturity_code) != 9),
+            unique_fe = length(unique(fishing_event_id)),
+            min_age_year = min(age_years, na.rm = TRUE),
+            max_age_year = max(age_years, na.rm = TRUE)
+            )
+
+
+xx3 <- d2  |>
+  group_by(survey_series_id, survey_series_desc) |>
+  mutate(age_years = ifelse(!is.na(age), year, NA)) |>
+  summarise(n = n(),
+            lengths = length(na.omit(length)),
+            weights = length(na.omit(weight)),
+            ages = length(na.omit(age)),
+            maturities = sum(na.omit(maturity_code) != 9),
+            unique_fe = length(unique(fishing_event_id)),
+            min_age_year = min(age_years, na.rm = TRUE),
+            max_age_year = max(age_years, na.rm = TRUE)
+  )
+
+
 
 dd <- x[duplicated(x$specimen_id),]
-dd1 <- filter(x, (specimen_id %in% c(dd$specimen_id)))
+dd2 <- filter(x, (specimen_id %in% c(dd$specimen_id)))
 
 
-xx3 <- x |>
+xx3 <- dd2 |>
   group_by(fn, ssid, species, survey_series_id, major_stat_area_code
            #, usability_desc
            ) |>
@@ -121,6 +168,8 @@ check <- dset$x |> filter(fn == 2 & ssid == 7)
 
 # Unlike values
 u <- dset$u
+u <- dsam$u
 
 # Summary of returns
 s <- dset$s
+s <- dsam$s
