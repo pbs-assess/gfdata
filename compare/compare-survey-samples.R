112d9f877398ceb90d0d69c5b7dea4abd0cee4a3
Rogers <Luke.Rogers@dfo-mpo.gc.ca>
2024-08-29 12:27:57 -0700

    compare 5 species subset; store alldiff results

--- compare/compare-survey-samples.R
+++ compare/compare-survey-samples.R
@@ -1,8 +1,9 @@
 # Overview
-# - Iterates over species and ssid to call both get_survey_samples*()
+# - Iterates over species and ssid to call both get_*survey_samples()
 # - Stores extra speciments relative to other function (x)
 # - Stores unlike rows/repetitions for same specimen id (u)
 # - Stores summary of returns (s)
+# - Stores all specimens when any differed (a)
 
 # Load packages ----------------------------------------------------------------
 
@@ -16,7 +17,10 @@ ssid <- id_tab |> dplyr::pull(ssid)
 
 # Get species ------------------------------------------------------------------
 
-spp <- readRDS("compare/data/species.rds")
+spp <- readRDS("compare/data/species.rds")[c(1, 5, 6, 8, 10)]
+
+# Print
+cat("spp = \n", paste0(spp, "\n"))
 
 # Shorter species set
 # spp <- "225" # Hake
@@ -25,7 +29,7 @@ spp <- readRDS("compare/data/species.rds")
 
 # Compare survey samples -------------------------------------------------------
 
-d <- compare_survey_samples(spp = spp, ssid = ssid, areas = areas)
+d <- compare_survey_samples(spp = spp, ssid = ssid, areas = NULL)
 
 # Extra specimens
 x <- d$x
@@ -36,8 +40,12 @@ u <- d$u
 # Summary of returns
 s <- d$s
 
+# All specimens when any differed
+a <- d$a
+
 # Write results ----------------------------------------------------------------
 
 saveRDS(x, file = "compare/results/samples-extras.rds")
 saveRDS(u, file = "compare/results/samples-unlike.rds")
 saveRDS(s, file = "compare/results/samples-summary.rds")
+saveRDS(a, file = "compare/results/samples-alldiff.rds")
--- compare/compare-survey-sets.R
+++ compare/compare-survey-sets.R
@@ -1,8 +1,9 @@
 # Overview
-# - Iterates over species and ssid to call both get_survey_sets*()
+# - Iterates over species and ssid to call both get_*survey_sets()
 # - Stores extra sets relative to other function (x)
 # - Stores unlike rows/repetitions for same comparison id (u)
 # - Stores summary of returns (s)
+# - Stores all sets when any differed (a)
 
 # Load packages ----------------------------------------------------------------
 
@@ -16,7 +17,10 @@ ssid <- id_tab |> dplyr::pull(ssid)
 
 # Get species ------------------------------------------------------------------
 
-spp <- readRDS("compare/data/species.rds")
+spp <- readRDS("compare/data/species.rds")[c(1, 5, 6, 8, 10)]
+
+# Print
+cat("spp = \n", paste0(spp, "\n"))
 
 # Shorter species set
 # spp <- "225" # Hake
@@ -36,8 +40,12 @@ u <- d$u
 # Summary of returns
 s <- d$s
 
+# All sets when any differed
+a <- d$a
+
 # Write results ----------------------------------------------------------------
 
 saveRDS(x, file = "compare/results/sets-extras.rds")
 saveRDS(u, file = "compare/results/sets-unlike.rds")
 saveRDS(s, file = "compare/results/sets-summary.rds")
+saveRDS(a, file = "compare/results/sets-alldiff.rds")
