9f41018b07ae88900d6f05d9e7d885b30908b754
Rogers <Luke.Rogers@dfo-mpo.gc.ca>
2024-09-12 15:14:49 -0700

    fixed notation

--- compare/compare-all.R
+++ compare/compare-all.R
@@ -9,12 +9,12 @@ library(tidyverse)
 
 # Read samples files
 xa <- readRDS("compare/results/samples-extras.rds")
-va <- readRDS("compare/results/samples-unlike.rds")
+ua <- readRDS("compare/results/samples-unlike.rds")
 sa <- readRDS("compare/results/samples-summary.rds")
 aa <- readRDS("compare/results/samples-alldiff.rds")
 
 # Read sets files
 xe <- readRDS("compare/results/sets-extras.rds")
-ve <- readRDS("compare/results/sets-unlike.rds")
+ue <- readRDS("compare/results/sets-unlike.rds")
 se <- readRDS("compare/results/sets-summary.rds")
 ae <- readRDS("compare/results/sets-alldiff.rds")
