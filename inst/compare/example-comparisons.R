f4b241ab5ffc855814ba7f0a4b356d67de2c6fb5
ecophilina <paenglis@lakeheadu.ca>
2024-10-09 13:30:39 -0700

    minor vignette and comparison edits

--- inst/compare/example-comparisons.R
+++ inst/compare/example-comparisons.R
@@ -1,21 +1,26 @@
 # Load
-devtools::load_all()
+library(gfdata)
+library(tidyverse)
+
 # Source functions
 source(here::here("inst", "compare", "compare-survey-sets.R"))
 source(here::here("inst", "compare", "compare-survey-samples.R"))
+
 # Define species and ssids
 spp <- c("Yelloweye Rockfish", "Eulachon")
-ssids <-  c(#1,2,7,14,
-            22)
+
 # ssids <- NULL
+ssids <-  c(1,2,7,14,22) # shortlist spanning common types of surveys
+# ssids <- c(1,2,3,4,6,7,14,16,22,36,39,40) # full list of gfsynopsis surveys
+# ssids <- c(35, 68, 76, 82:87) # surveys excluded from gfsynopsis sablefish, hake, dogfish and jig
 
 # Compare survey sets
 de <- compare_survey_sets(spp = spp,
-                         ssids = ssids # NULL here (default) returns default ssid for og fn
-                         # # Args for get_all_survey_sets()
-                         # remove_false_zeros = FALSE, # not default
-                         # usability = c(0, 1, 2, 6) # not default
-                         # grouping_only = TRUE # not default
+                         ssids = ssids, # NULL here (default) returns default ssids for og fn
+                         ## settings for get_all function that match more closely behaviour of og fn
+                         # usability = c(0, 1, 2, 6), # not default
+                         # grouping_only = TRUE, # not default
+                         remove_false_zeros = FALSE # not default
                          )
 
 # Extra events for a given species-survey combination
@@ -34,8 +39,12 @@ de$a
 # Compare survey samples
 ds <- compare_survey_samples(spp = spp,
                             ssids = ssids, # default is NULL for both
-                            major = NULL, # default is NULL for both
-                            usability = NULL # default is NULL for both
+                            # major = NULL, # default is NULL for both
+                            usability = c(0, 1, 2, 6), # default is NULL for both
+                            unsorted_only = TRUE, # not default
+                            random_only = TRUE, # not default
+                            grouping_only = TRUE,
+                            drop_na_columns = FALSE # not default
                             )
 
 # Extra samples for a given species-survey combination
