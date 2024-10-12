0df2af1d0691c8b0eafed539b82dfd70ff51ce95
English <Philina.English@dfo-mpo.gc.ca>
2024-10-11 17:00:26 -0700

    precompile option for get-all vignette

---
+++ vignettes/precompile.R
@@ -0,0 +1,3 @@
+# Pre-compiled vignettes that take a long time to build and require network access
+knitr::knit("vignettes/gfdata-vignette-get-all.Rmd.orig", output = "vignettes/gfdata-vignette-get-all.Rmd")
+
