0f067d903b5cb17cc8da95a92837ed9f4d7fa9d5
ecophilina <paenglis@lakeheadu.ca>
2024-10-15 17:13:36 -0700

    rename rmd files [skip ci]

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,5 +1,5 @@
 # Pre-compiled vignettes that take a long time to build and require network access
 # Only run this if you're on network, otherwise you will overwrite the data stored in the the output file.
-knitr::knit("vignettes/gfdata-vignette.Rmd.orig", output = "vignettes/gfdata-vignette.Rmd")
-knitr::knit("vignettes/gfdata-vignette-get-all.Rmd.orig", output = "vignettes/gfdata-vignette-get-all.Rmd")
+knitr::knit("vignettes/gfdata-vignette.Rmd.orig", output = "vignettes/01-gfdata-vignette.Rmd")
+knitr::knit("vignettes/gfdata-vignette-get-all.Rmd.orig", output = "vignettes/02-gfdata-vignette-get-all.Rmd")
 
