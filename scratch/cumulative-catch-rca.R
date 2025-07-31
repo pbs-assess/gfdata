a9eb7bd51888ba08a9657c1f31db6cb270f1bccd
Sean Anderson <sean@seananderson.ca>
2025-07-31 14:08:37 -0700

    Update cumulative spatial catch script

--- scratch/cumulative-catch-rca.R
+++ scratch/cumulative-catch-rca.R
@@ -23,6 +23,7 @@ dat_rca_cpue <- select(d, species, date, longitude, latitude, rca_establishment)
   ) |>
   st_transform(st_crs(32609))
 dat_rca_cpue$date <- lubridate::ymd(dat_rca_cpue$date)
+dat_rca_cpue$ccira_row_id <- 1:nrow(dat_rca_cpue)
 
 coast_utm <- pacea::bc_coast |>
   st_transform(crs = 32609) |>
@@ -32,7 +33,7 @@ ggplot(dat_rca_cpue) +
   geom_sf() +
   geom_sf(data = coast_utm)
 
-catch <- readRDS("catch-spatial.rds")
+catch_raw <- readRDS("spatial_catch_2025_07_31.rds")
 
 weights <- readr::read_csv("species_weights_1.csv") |> select(-1)
 weights$species_dfo <- ifelse(!is.na(weights$species_dfo), weights$species_dfo, paste(weights$species, "rockfish"))
@@ -69,18 +70,141 @@ test <- filter(catch_data, is.na(median_weight)) |>
   table()
 stopifnot(nrow(test) == 0L)
 
-trawl_catch <- catch_data |>
-  filter(gear %in% c("BOTTOM TRAWL", "UNKNOWN TRAWL")) |>
-  mutate(catch = discarded_kg + landed_kg)
+table(catch_data$gear)
+table(catch_data$gear, catch_data$year)
 
-midwater_catch <- catch_data |>
-  filter(gear == "MIDWATER TRAWL") |>
+theme_set(theme_light())
+
+line_year <- 2006
+g1 <- catch_data |> 
+  group_by(year, gear) |>  
+  summarize(discarded_kg = sum(discarded_kg)) |> 
+  ggplot(aes(year, discarded_kg)) + geom_col() +
+  geom_vline(xintercept = c(line_year), lty = 2) +
+  facet_wrap(~gear, scales = "free_y")
+
+g2 <- catch_data |> 
+  group_by(year, gear) |>  
+  summarise(discarded_pcs = sum(discarded_pcs)) |> 
+  ggplot(aes(year, discarded_pcs)) + geom_col() +
+  geom_vline(xintercept = c(line_year), lty = 2) +
+  facet_wrap(~gear, scales = "free_y")
+
+cowplot::plot_grid(g1, g2, ncol = 1)
+ggsave("discarded-by-gear.pdf", width = 9, height = 9)
+
+g1 <- catch_data |> 
+  group_by(year, gear) |>  
+  summarize(landed_kg = sum(landed_kg)) |> 
+  ggplot(aes(year, landed_kg)) + geom_col() +
+  geom_vline(xintercept = c(line_year), lty = 2) +
+  facet_wrap(~gear, scales = "free_y")
+
+g2 <- catch_data |> 
+  group_by(year, gear) |>  
+  summarize(landed_pcs = sum(landed_pcs)) |> 
+  ggplot(aes(year, landed_pcs)) + geom_col() +
+  geom_vline(xintercept = c(line_year), lty = 2) +
+  facet_wrap(~gear, scales = "free_y")
+
+cowplot::plot_grid(g1, g2, ncol = 1)
+ggsave("landed-by-gear.pdf", width = 9, height = 9)
+
+g1 <- catch_data |> 
+  filter(gear %in% c("HOOK AND LINE", "TRAP")) |> 
+  group_by(year, species_common_name) |>
+  summarize(landed_kg = sum(landed_kg)) |> 
+  ggplot(aes(year, landed_kg)) + geom_col() +
+  geom_vline(xintercept = c(line_year), lty = 2) +
+  facet_wrap(~species_common_name, scales = "free_y")
+
+g2 <- catch_data |> 
+  filter(gear %in% c("HOOK AND LINE", "TRAP")) |> 
+  group_by(year, species_common_name) |> 
+  summarize(landed_pcs = sum(landed_pcs)) |> 
+  ggplot(aes(year, landed_pcs)) + geom_col() +
+  geom_vline(xintercept = c(line_year), lty = 2) +
+  facet_wrap(~species_common_name, scales = "free_y")
+
+cowplot::plot_grid(g1, g2, ncol = 1)
+ggsave("landed-by-sp.pdf", width = 10, height = 10)
+
+g1 <- catch_data |> 
+  filter(gear %in% c("HOOK AND LINE", "TRAP")) |> 
+  group_by(year, species_common_name) |> 
+  summarize(discarded_kg = sum(discarded_kg)) |> 
+  ggplot(aes(year, discarded_kg)) + geom_col() +
+  geom_vline(xintercept = c(line_year), lty = 2) +
+  facet_wrap(~species_common_name, scales = "free_y")
+
+g2 <- catch_data |> 
+  filter(gear %in% c("HOOK AND LINE", "TRAP")) |> 
+  group_by(year, species_common_name) |>
+  summarize(discarded_pcs = sum(discarded_pcs)) |> 
+  ggplot(aes(year, discarded_pcs)) + geom_col() +
+  geom_vline(xintercept = c(line_year), lty = 2) +
+  facet_wrap(~species_common_name, scales = "free_y")
+
+cowplot::plot_grid(g1, g2, ncol = 1)
+ggsave("discarded-by-sp.pdf", width = 10, height = 10)
+
+g1 <- catch_data |> 
+  filter(gear %in% c("HOOK AND LINE", "TRAP")) |> 
+  group_by(year, species_common_name) |>
+  summarize(discarded_kg_per_pc = sum(discarded_kg)/sum(discarded_pcs)) |> 
+  ggplot(aes(year, discarded_kg_per_pc)) + geom_line() +
+  geom_vline(xintercept = c(line_year), lty = 2) +
+  facet_wrap(~species_common_name, scales = "free_y")
+g1
+ggsave("discarded-kg-per-pc.pdf", width = 10, height = 5)
+
+g1 <- catch_data |> 
+  filter(gear %in% c("HOOK AND LINE", "TRAP")) |> 
+  group_by(year, species_common_name) |>
+  summarize(landed_kg_per_pc = sum(landed_kg)/sum(landed_pcs)) |> 
+  ggplot(aes(year, landed_kg_per_pc)) + geom_line() +
+  geom_vline(xintercept = c(line_year), lty = 2) +
+  facet_wrap(~species_common_name, scales = "free_y")
+g1
+ggsave("landed-kg-per-pc.pdf", width = 10, height = 5)
+
+g1 <- catch_data |> 
+  filter(gear %in% c("HOOK AND LINE", "TRAP"), year >= 2006) |> 
+  group_by(species_common_name) |>
+  # summarize(landed_kg_per_pc = sum(landed_kg)/sum(landed_pcs), median_weight_input = mean(median_weight)) |> 
+  summarize(landed_kg_per_pc = median(landed_kg/landed_pcs, na.rm = TRUE), median_weight_input = mean(median_weight)) |> 
+  ggplot(aes(landed_kg_per_pc, median_weight_input)) + 
+  geom_abline(intercept = 0, slope = 1, lty = 2) +
+  geom_point() +
+  ggrepel::geom_text_repel(aes(label = species_common_name),  max.overlaps = 100) +
+  scale_y_log10() + scale_x_log10() +
+  coord_equal(ylim = c(0.1, 25), xlim = c(0.1, 25))
+g1
+ggsave("hook-and-line-trap-weights.pdf", width = 7, height = 7)
+
+new_weights <- catch_data |> 
+  filter(gear %in% c("HOOK AND LINE", "TRAP"), year >= 2006) |> 
+  group_by(species_common_name) |>
+  summarize(median_weight = median(landed_kg/landed_pcs, na.rm = TRUE)) |>
+  as.data.frame() |> select(species_common_name, median_weight)
+new_weights
+
+catch_data$median_weight <- NULL
+catch_data <- left_join(catch_data, new_weights)
+
+trawl_catch <- catch_data |>
+  filter(gear %in% c("BOTTOM TRAWL", "UNKNOWN TRAWL", "MIDWATER TRAWL")) |>
   mutate(catch = discarded_kg + landed_kg)
 
 ll_catch <- catch_data |>
-  # filter(year >= 2006) |>
   filter(gear %in% c("HOOK AND LINE", "LONGLINE", "TRAP")) |>
-  mutate(catch = ifelse(year < 2006, discarded_kg + landed_kg, (discarded_pcs + landed_pcs) * median_weight))
+  mutate(
+    catch = ifelse(
+      year < 2006, 
+      discarded_kg + landed_kg, 
+      discarded_pcs * median_weight + landed_kg
+    )
+  )
 
 ll_catch |>
 # midwater_catch |>
@@ -98,7 +222,7 @@ ll_catch |>
   scale_size_area() +
   scale_colour_viridis_c()
 
-combined_catch <- bind_rows(trawl_catch, midwater_catch, ll_catch) |>
+combined_catch <- bind_rows(trawl_catch, ll_catch) |>
   filter(best_date < max(dat_rca_cpue$date)) |>
   select(year, best_date, species_common_name, catch, gear, vessel_registration_number) |>
   filter(catch > 0)
@@ -110,8 +234,6 @@ get_radius(10)
 pi * 2^2
 
 find_catch_within_distance <- function(dat_rca_cpue_row, input_catch_data, distance = 1500) {
-  # sum catch within X m of each point
-
   cat(dat_rca_cpue_row, "\n")
 
   sf1 <- dat_rca_cpue[dat_rca_cpue_row, , drop = FALSE]
@@ -169,11 +291,14 @@ find_catch_within_distance <- function(dat_rca_cpue_row, input_catch_data, dista
   left_join(sf1, summary_df, by = "id")
 }
 
+glimpse(dat_rca_cpue)
+dat_rca_cpue$median_weight <- NULL
+find_catch_within_distance(1, combined_catch) |> glimpse()
 find_catch_within_distance(2, combined_catch) |> glimpse()
 # find_catch_within_distance(11, combined_catch) |> glimpse()
 
 tictoc::tic()
-# out <- purrr::map_dfr(1:1000, \(i) find_catch_within_distance(i, combined_catch))
+# out <- purrr::map_dfr(1:10, \(i) find_catch_within_distance(i, combined_catch))
 # out <- furrr::future_map_dfr(1:1000, \(i) find_catch_within_distance(i, combined_catch))
 out <- furrr::future_map_dfr(1:nrow(dat_rca_cpue), \(i) find_catch_within_distance(i, combined_catch))
 tictoc::toc()
